24f22745e9c4c564671fb132cc1fa279049da080
KholoudAlShammari <k.alshamry@yahoo.com>
2017-04-03 15:31:44 +0300

    Kholoud's Last Programming Assignment 2 Submission Code

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,8 @@
 ## Name: Kholoud Al-Shammari
 ## Write a short comment describing this function
 
+
+## This makeCacheMatrix function is a function to create a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -21,6 +23,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+## The cacheSolve Function is a function to compute the inverse of the special "matrix" 
+## that returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
